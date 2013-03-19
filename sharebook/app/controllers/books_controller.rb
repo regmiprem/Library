4b3f8b34d4d6257ab91a3ba57f1e14c3128a05d6
Prem Regmi <prem.regmi@braindigit.com>
2013-03-19 16:58:06 +0545

    changes

---
+++ sharebook/app/controllers/books_controller.rb
@@ -0,0 +1,95 @@
+class BooksController < ApplicationController
+  # GET /books
+  # GET /books.json
+  helper_method :sort_column, :sort_direction
+  add_breadcrumb "Home", :dashboards_path
+  add_breadcrumb "My Books", :books_path
+  def index
+    @books = Book.search(params[:search]).order_by(sort_column + " " + sort_direction).page(params[:page]).per(5).where(:user_id => current_user.id)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @books }
+    end
+  end
+
+  # GET /books/1
+  # GET /books/1.json
+  def show
+    @book1 = Book.new
+    @book = Book.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @book }
+    end
+  end
+
+  # GET /books/new
+  # GET /books/new.json
+  def new
+    @book = Book.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @book }
+    end
+  end
+
+  # GET /books/1/edit
+  def edit
+    #add_breadcrumb "Edit", edit_books_path
+    @book = Book.find(params[:id])
+  end
+
+  # POST /books
+  # POST /books.json
+  def create
+    @book = Book.new(params[:book])
+
+    respond_to do |format|
+      if @book.save
+        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.json { render json: @book, status: :created, location: @book }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /books/1
+  # PUT /books/1.json
+  def update
+    @book = Book.find(params[:id])
+
+    respond_to do |format|
+      if @book.update_attributes(params[:book])
+        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /books/1
+  # DELETE /books/1.json
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    respond_to do |format|
+      format.html { redirect_to books_url }
+      format.json { head :no_content }
+    end
+  end
+    def sort_column
+    Book.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
